d5058a100758bc53b5a158b375e57eeb6ef864d4
tinski <t.bc.kaminski@gmail.com>
2025-04-12 14:11:51 +0200

    Fix torch.load and logging calls

--- src/laion_clap/hook.py
+++ src/laion_clap/hook.py
@@ -116,7 +116,7 @@ class CLAP_Module(torch.nn.Module):
         if verbose:
             param_names = [n for n, p in self.model.named_parameters()]
             for n in param_names:
-                logging.info(n, "\t", "Loaded" if n in ckpt else "Unloaded")
+                logging.info(f"{n} \t {'Loaded' if n in ckpt else 'Unloaded'}")
     
     def get_audio_embedding_from_filelist(self, x, use_tensor=False):
         """get audio embeddings from the audio file list
